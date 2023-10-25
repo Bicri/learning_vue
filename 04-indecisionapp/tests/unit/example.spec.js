93e675a062a523e52ad5e85283d2fd0c8f23a623
Bicri <isaac.bicri@gmail.com>
2023-10-25 15:50:38 -0600

    inicio de pruebas y cambio de versión de node

--- 04-indecisionapp/tests/unit/example.spec.js
+++ 04-indecisionapp/tests/unit/example.spec.js
@@ -1,12 +1,17 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+describe('Example Componenet', () => {
+  test( 'Debe ser mayor a 10', () => {
+    
+    //Arreglar
+    let value = 5;
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+    //Estimulo
+    value += 2;
+
+    //Aserción
+    if(value > 10){
+      //TODO bien
+    } else {
+      throw `${value} no es mayor a 10`
+    }
   })
 })
